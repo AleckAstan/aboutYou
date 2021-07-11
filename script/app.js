f886b218a81abc346c6fab615b9889908e94542b
Aleck <astanaleck@gmail.com>
2021-07-11 23:02:08 +0000

    ContainerDone

--- script/app.js
+++ script/app.js
@@ -1,20 +1,24 @@
 let mode = 0;
-let flip = document.getElementById("flip");
+let cardBox = document.getElementById("cardBox");
 let card = document.getElementById("card");
 let cardContent = document.getElementById("cardContent");
 let sideways = document.getElementById("sideways");
 
 let anime = function animate() {
     if (mode == 0) {
-        card.style.transform = 'scale(2) rotateX(180deg)';
+        card.style.transform = 'scaleX(2.5) scaleY(2.2) rotateX(180deg)';
+        card.style.opacity = '1';
         cardContent.style.transform = 'rotateX(180deg)';
-        cardContent.style.transition = 'ease 0.5s'
+        cardContent.style.transition = 'ease 0.4s'
         cardContent.style.opacity = '0';
         sideways.style.transition = 'ease 2s'
         sideways.style.opacity = '1';
         mode = 1;
     } else {
         card.style.transform = 'scale(1) rotateX(0deg)';
+
+        card.style.transition = 'ease 1s'
+        card.style.opacity = '0';
         cardContent.style.transform = 'rotateX(0deg)';
         cardContent.style.transition = 'ease 1s'
         cardContent.style.opacity = '1';
@@ -28,4 +32,4 @@ let anime = function animate() {
 }
 
 
-flip.addEventListener('click', anime, false);
+cardBox.addEventListener('click', anime, false);
